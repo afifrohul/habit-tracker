6d48e22adb756c833c987b4207496d1db4865c3c
afifrohul <afifmemyself22@gmail.com>
2025-12-07 20:15:17 +0700

    update card dashboard

--- resources/js/components/dashboard-card.tsx
+++ resources/js/components/dashboard-card.tsx
@@ -1,5 +1,7 @@
 import { LucideIcon } from 'lucide-react';
 
+import { Card } from '@/components/ui/card';
+
 export default function DashboardCard({
     header,
     icon: Icon,
@@ -14,21 +16,23 @@ export default function DashboardCard({
     className?: string;
 }) {
     return (
-        <div className={`rounded border p-6 ${className ?? ''}`}>
-            <div>
-                <div className="flex items-center justify-between">
-                    <p className="text-sm text-neutral-500 dark:text-neutral-300">
-                        {header}
-                    </p>
-                    <div>
-                        {Icon && (
-                            <Icon className="text-sm text-neutral-700 dark:text-neutral-500" />
-                        )}
+        <Card className='p-4'>
+            <div className={`rounded ${className ?? ''}`}>
+                <div>
+                    <div className="flex items-center justify-between">
+                        <p className="text-sm text-neutral-500 dark:text-neutral-300">
+                            {header}
+                        </p>
+                        <div>
+                            {Icon && (
+                                <Icon className="text-sm text-neutral-700 dark:text-neutral-500" />
+                            )}
+                        </div>
                     </div>
+                    <p className="mt-2 text-2xl font-extrabold">{data}</p>
+                    <p className="mt-4 text-sm font-medium">{footer}</p>
                 </div>
-                <p className="mt-2 text-2xl font-extrabold">{data}</p>
-                <p className="mt-4 text-sm font-medium">{footer}</p>
             </div>
-        </div>
+        </Card>
     );
 }
